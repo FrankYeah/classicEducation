782230dc907d241bce1876180d41f15062b7421e
FrankYeah <aaa24295234@gmail.com>
2021-08-17 21:30:21 +0800

    rotate

---
+++ js/content.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    
+    thumbs: {
+      swiper: swiper,
+    },
+  });
