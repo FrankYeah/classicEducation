0573423480aebdad1ff26e837b12baeeca7d4063
FrankYeah <aaa24295234@gmail.com>
2021-08-18 15:18:38 +0800

    header

--- js/common.js
+++ js/common.js
@@ -35,3 +35,35 @@ header3.addEventListener('mouseleave', function() {
   menu3.style.display = 'none';
 });
 
+// menu
+
+var btn = document.querySelector('.header-search-icon-menu');
+btn.addEventListener('click', function() {
+  location.href = "./result.html?search=" + document.querySelector(".header-input-menu").value
+});
+
+var menuOut = document.getElementById('menuOut');
+var menuInBox = document.getElementById('menuInBox');
+var menuIn = document.getElementById('menuIn');
+
+menuOut.addEventListener('click', function() {
+  menuInBox.style.display = 'block';
+});
+
+menuIn.addEventListener('click', function() {
+  menuInBox.style.display = 'none';
+});
+
+// menu 裡面
+
+$("#headerRwd1").click(function(){
+  $("#menuRwd1").toggle()
+})
+
+$("#headerRwd2").click(function(){
+  $("#menuRwd2").toggle()
+})
+
+$("#headerRwd3").click(function(){
+  $("#menuRwd3").toggle()
+})
