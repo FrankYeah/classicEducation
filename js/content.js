9fc4c220161c16e6ef46f2bb890bcf2df0013d21
FrankYeah <aaa24295234@gmail.com>
2021-08-18 22:58:35 +0800

    fix

--- js/content.js
+++ js/content.js
@@ -1,4 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = null
+var swiper2 = null
+
+if(screen.width > 1024) {
+  swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
@@ -9,10 +13,35 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
       },
   });
-  var swiper2 = new Swiper(".mySwiper2", {
+
+  swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     
     thumbs: {
       swiper: swiper,
     },
   });
+} else {
+
+  swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 2,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+}
+
